44ab6b828bad07911cbc00f1ff22b87773876fd7
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-03-29 09:16:43 +0530

    made some validation in AddUser file

--- src/components/User/AddUsers.js
+++ src/components/User/AddUsers.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../UI/Button';
 import Card from '../UI/Card';
 import styles from './AddUser.module.css'
 
 const AddUser = () => {
+    const [enteredUsername,setEnterendUsename]=useState('');
+    const [enteredAge,setEnterendAge]=useState('');
     const addUserHandler=(event)=>{
         event.preventDefault();
+        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+            return;
+        }
+        if(+enteredAge <1){ //this enterAge Was strin but this + made it number
+            return ;
+        }
+        console.log(enteredAge,enteredUsername)
+        setEnterendUsename('');
+        setEnterendAge('');
     }
+    const usernameChangeHandler=(event)=>{
+        setEnterendUsename(event.target.value);
+    }
+
+    const ageChangeHandler=(event)=>{
+        setEnterendAge(event.target.value);
+    }
+
     return (
     <Card className={styles.input} >
         <form onSubmit={addUserHandler}>
             <label htmlFor='username'>Username</label>
-            <input id='username' type='text' />
+            <input id='username' onChange={usernameChangeHandler} value ={enteredUsername} type='text' />
             <label htmlFor='age'>Age (Years) </label>
-            <input id='age' type='number' />
+            <input id='age' onChange={ageChangeHandler} value={enteredAge} type='number' />
             <Button type='submit'>Add User</Button>
         </form>
     </Card>
