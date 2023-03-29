378203b56ff8f02355fa6e0fc0fbf3480bf3eb5f
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-03-29 18:30:12 +0530

    Added Wrapper to replace div (Helpers)

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Wrapper from '../Helpers/Wrapper'
 import Button from './Button'
 import Card from './Card'
 import styles from './ErrorModal.module.css'
 
 const ErrorModal = (props) => {
   return (
-    <div>
+    <Wrapper>
         <div className={styles.backdrop} onClick={props.onConfirm} />
     <Card className={styles.modal}>
         <header className={styles.header}>
@@ -18,7 +19,7 @@ const ErrorModal = (props) => {
             <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
     </Card>
-    </div>
+    </Wrapper>
   )
 }
 
